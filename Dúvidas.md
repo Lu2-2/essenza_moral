901b4c8944018979174e33cc05700b28507add58
Luis <luis.henriquenc10@gmail.com>
2025-10-04 16:37:13 -0300

    Quase finalizado

--- Dúvidas.md
+++
@@ -1,1 +0,0 @@
-Clonar -> git clone {nome do repositorio (sem as chaves)}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const risotto = document.querySelector('#risotto');
+const ossobuco = document.querySelector('#ossobuco');
+const tagliolini = document.querySelector('#tagliolini');
+const fiorentina = document.querySelector('#fiorentina');
+const parmigiana = document.querySelector('#parmigiana');
