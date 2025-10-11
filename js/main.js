0346578a4fbad9749132d187e2e0188bbaa5b323
Luis <luis.henriquenc10@gmail.com>
2025-10-11 14:57:32 -0300

    Inicio ok, Unidade ok, arrumar cardápio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-const risotto = document.querySelector('#risotto');
-const ossobuco = document.querySelector('#ossobuco');
-const tagliolini = document.querySelector('#tagliolini');
-const fiorentina = document.querySelector('#fiorentina');
-const parmigiana = document.querySelector('#parmigiana');
+const caixa = document.querySelector('#caixa');
+const botoes = document.querySelectorAll('button[data-prato]');
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    const prato = botao.dataset.prato;
+
+    // aplica o fade-out
+    caixa.classList.add('fade-out');
+
+    // troca a imagem após o fade-out
+    setTimeout(() => {
+      caixa.src = `./images/pratos/itens/${prato}.png`;
+
+      // espera um pouco a imagem carregar e aplica fade-in
+      caixa.onload = () => {
+        caixa.classList.remove('fade-out');
+      };
+    }, 300); // tempo antes de trocar (metade da transição)
+  });
+});
