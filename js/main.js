bb0dd0baa31a5c19df1e60cd7f45bfef229b5451
Luis <luis.henriquenc10@gmail.com>
2025-10-18 14:47:59 -0300

    inicio ok

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const caixa = document.querySelector('#caixa');
 const botoes = document.querySelectorAll('button[data-prato]');
+const h1 = document.querySelector('#caixa_texto')
 
 botoes.forEach(botao => {
   botao.addEventListener('click', () => {
     const prato = botao.dataset.prato;
+    h1.innerHTML = `${prato}`;
 
     // aplica o fade-out
     caixa.classList.add('fade-out');
+    h1.classList.add('h1_animation');
 
     // troca a imagem após o fade-out
     setTimeout(() => {
@@ -15,7 +18,8 @@ botoes.forEach(botao => {
       // espera um pouco a imagem carregar e aplica fade-in
       caixa.onload = () => {
         caixa.classList.remove('fade-out');
+        h1.classList.remove('h1_animation')
       };
-    }, 300); // tempo antes de trocar (metade da transição)
+    }, 400); // tempo antes de trocar (metade da transição)
   });
 });
