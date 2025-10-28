e8e0da6d5b7b24e01a2743f9c2429f05141dbf7b
Luis <luis.henriquenc10@gmail.com>
2025-10-28 20:18:09 -0300

    Js

---
+++ pages/reserva/reserva.js
@@ -0,0 +1,14 @@
+const nome = document.querySelector('#form_name');
+const dia = document.querySelector('#form_date');
+const hora = document.querySelector('#form_time');
+const formulario = document.querySelector('#meu-form');
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault(); // Importante!
+    
+    console.log({
+        nome: nome.value,
+        dia: dia.value,
+        hora: hora.value
+    });
+});
